fcd749d474178c5b8b16371cc6b1ac667984e290
Kevin Koenen <k.koenen93@gmail.com>
2020-05-12 10:12:55 +0200

    Removed Transporters codes array

--- src/Models/Transport.php
+++ src/Models/Transport.php
@@ -22,14 +22,6 @@ class Transport
     public $shipmentReference;
 
     /**
-     * The key is de Bol.com shipment name.
-     * The values will be mapped to the correct shipment name.
-     *
-     * @var array
-     */
-    protected $transportCodes = [];
-
-    /**
      * TransportItem constructor.
      * @param string $transporterCode
      * @param string $trackingCode
