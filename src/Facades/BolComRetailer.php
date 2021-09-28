d39b2305cf37278fcc4e92a39526bc8b82c94ca7
Kevin Koenen <k.koenen93@gmail.com>
2021-09-28 10:40:59 +0200

    Fixed OrderItem -> OrderOrderItem

--- src/Facades/BolComRetailer.php
+++ src/Facades/BolComRetailer.php
@@ -6,14 +6,14 @@ use HomeDesignShops\LaravelBolComRetailer\Models\Transport;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 use Picqer\BolRetailer\Model\Order;
-use Picqer\BolRetailer\Model\OrderItem;
+use Picqer\BolRetailer\Model\OrderOrderItem;
 use Picqer\BolRetailer\Model\ProcessStatus;
 
 /**
  * @see \HomeDesignShops\LaravelBolComRetailer\BolComRetailerService
  *
  * @method static Collection getOpenOrders()
- * @method static ProcessStatus shipOrderItem(OrderItem $orderItem, Transport $transport)
+ * @method static ProcessStatus shipOrderItem(OrderOrderItem $orderItem, Transport $transport)
  * @method static Order|null getOrder(string $orderId)
  */
 class BolComRetailer extends Facade
